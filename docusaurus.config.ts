b8cc6c3400a70d733e3f7071f8f2e5163169c72c
mihailhiminec <varstt@ya.ru>
2024-10-07 19:43:54 +0500

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Praweb-Union', // Usually your GitHub org/user name.
-  projectName: 'ck-docs', // Usually your repo name.
+  projectName: '', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
