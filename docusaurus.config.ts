b8d1084f3e55dcc18db7880fc2eb5da4c77537a1
mihailhiminec <varstt@ya.ru>
2024-10-07 19:18:41 +0500

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -66,7 +66,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Dinosaurs are cool',
+      title: 'Центр компетенций',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
