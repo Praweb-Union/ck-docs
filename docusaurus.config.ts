46e4b62af51261075484062a70b76ca9d7c641a1
mihailhiminec <varstt@ya.ru>
2024-10-07 18:52:53 +0500

    test

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -11,8 +11,8 @@ const config: Config = {
   url: 'https://github.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
-
+  baseUrl: '/ck-docs/',
+  trailingSlash: false,
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'Praweb-Union', // Usually your GitHub org/user name.
